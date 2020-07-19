dea39da82bf1767070b6610504bfdeb4f0e2d07c
Paola Garay <garay.angela@gmail.com>
2020-07-19 16:00:25 +0200

    iteration 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import users from "./users.json";
+import UsersList from "./components/UsersList"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    users: users.slice(0, 80),
+  }
+
+  render(){
+    return(
+      <div>
+      <h1>Ironbook</h1>
+
+      <UsersList
+      users={this.state.users}
+      />
+
+      </div>
+    )
+  }
+
 }
 
 export default App;
---
+++ src/components/UsersList.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const UsersList = props =>{
+   
+    return(
+        <div style={{ display: 'flex', justifyContent: 'center'}}>
+            <table>
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Campus</th>
+                        <th>Role</th>
+                        <th>Linkedin</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.users.map( user => {
+                        return (
+                            <tr>
+                                <td>{ user.firstName }</td>
+                                <td>{ user.lastName }</td>
+                                <td>{ user.campus }</td>
+                                <td> { user.role} </td>
+                                <td> { user.linkedin} </td>
+                            </tr>
+                        )
+                    })}
+
+                </tbody>
+            </table>
+        </div>
+    )
+  
+}
+
+export default UsersList;
